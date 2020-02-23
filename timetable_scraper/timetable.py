0590e6141a17f619518230644bbf1f3677391d6e
Gergő Szabó <szg0219@gmail.com>
2020-02-23 09:54:00 +0100

    finalized print_classes_in_order

--- timetable_scraper/timetable.py
+++ timetable_scraper/timetable.py
@@ -1,27 +1,31 @@
+from datetime import datetime
+
 from lesson import Lesson
 
 
 class Timetable:
 
     def __init__(self):
-        self.classes = []
+        self.lessons = []
 
     @property
     def subject_codes(self):
-        return set([class_.subject_code for class_ in self.classes])
+        return set([class_.subject_code for class_ in self.lessons])
 
     def add_class(self, class_):
         if isinstance(class_, Lesson):
-            self.classes.append(class_)
+            self.lessons.append(class_)
         else:
             raise Exception
 
     def print_classes_in_order(self):
-        for class_ in self.classes:
+        ordered_classes = self.lessons
+        ordered_classes.sort(key=lambda lesson: datetime.strptime(lesson.sortable_date, '%Y %m %d'))
+        for class_ in ordered_classes:
             print(class_)
 
     def print_classes_by_subjects(self):
         for subject_code in self.subject_codes:
-            for class_ in self.classes:
+            for class_ in self.lessons:
                 if class_.subject_code == subject_code:
                     print(class_)
