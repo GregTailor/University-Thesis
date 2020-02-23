b38bfac22b354aa2a686fa4283918b2475dbb03a
Gergő Szabó <szg0219@gmail.com>
2020-02-23 09:54:47 +0100

    added major_filter property

--- timetable_scraper/student.py
+++ timetable_scraper/student.py
@@ -1,7 +1,14 @@
 class Student:
 
-    def __init__(self, name, subject_codes, year=()):
+    def __init__(self, name, subject_codes, year=(), major=None):
         self.name = name
         self.subject_codes = subject_codes
         self.timetable = None
         self.year = year
+        self.major = major
+
+    @property
+    def major_filter(self):
+        if self.major.lower() == 'pti bsc':
+            return [f'{year}. Programtervező informatikus BSc' for year in self.year]
+        return None
