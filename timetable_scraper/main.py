776c94ba12177d14f58f3e7316bb87250c8078fb
Gergő Szabó <szg0219@gmail.com>
2020-02-23 09:53:39 +0100

    added more information

--- timetable_scraper/main.py
+++ timetable_scraper/main.py
@@ -4,6 +4,9 @@ from scaper import TimetableScraper
 
 subject_codes = ['ILBPM9934', 'ILBPM9928', 'ILBPM9936', 'ILBPM9924', 'ILBPM0417',
                  'ILBPM0419', 'ILBPM0623', 'ILBPM9925', 'ILBPM0420', 'ILBPM0418']
-student = Student('Szabó Gergő', subject_codes)
+major = 'PTI BSC'
+year = (1, 2, 3)
+student = Student('Szabó Gergő', subject_codes, year, major)
 scraper = TimetableScraper(student)
+student.timetable.print_classes_in_order()
 student.timetable.print_classes_by_subjects()
